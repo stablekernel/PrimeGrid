8521dcd7524bd5ae2e9c9855f83674f98cde2161
Sean Swezey <sean.swezey@stablekernel.com>
2016-09-29 13:59:21 -0400

    Initial Commit

---
+++ PrimeGridLayout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PrimeGridLayout
+//
+//  Created by Sean Swezey on 9/29/16.
+//  Copyright © 2016 stable/kernel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
